fcf0c615ec571d6e50ca3288ab23f3f6e57253d7
jim_ <769925821@qq.com>
2017-12-22 17:21:44 +0800

    dir_reduc

---
+++ src/dir_reduc.rs
@@ -0,0 +1,42 @@
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
+}
+
+
+fn dir_reduc(arr: &[Direction]) -> Vec<Direction> {
+    let mut result: Vec<Direction> = vec![];
+    for a in arr {
+        let r = result.clone();
+        if let Some(_last) = r.last() {
+            if is_opposite(a, _last) {
+                result.pop().unwrap();
+                continue;
+            }
+        }
+        result.push(*a);
+    }
+    result
+}
+
+fn is_opposite(dir: &Direction, target: &Direction) -> bool {
+    let op = &match *dir {
+        Direction::NORTH => Direction::SOUTH,
+        Direction::SOUTH => Direction::NORTH,
+        Direction::EAST => Direction::WEST,
+        Direction::WEST => Direction::EAST,
+    };
+    op == target
+}
+
+#[test]
+fn returns_expected() {
+    use self::Direction::*;
+    let a = [NORTH, SOUTH, SOUTH, EAST, WEST, NORTH, WEST];
+    assert_eq!(dir_reduc(&a), [WEST]);
+    let a = [NORTH, WEST, SOUTH, EAST];
+    assert_eq!(dir_reduc(&a), [NORTH, WEST, SOUTH, EAST]);
+}
